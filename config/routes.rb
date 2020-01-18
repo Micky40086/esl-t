92a781f6cc36367d08770c2bed7af9d4f3f67652
Mickey <Micky40086@gmail.com>
2020-01-18 15:21:22 +0800

    feat: add homepage, config: image thumb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+
+  root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
