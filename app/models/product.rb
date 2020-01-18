ae37fa710b3d53987ecc97d0b9f6770258b09567
Mickey <Micky40086@gmail.com>
2020-01-18 14:14:02 +0800

    feat: add action text

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
-  has_many :product_images, inverse_of: :product
-  accepts_nested_attributes_for :product_images, reject_if: :all_blank, allow_destroy: true
+  has_rich_text :desc
 
   enum status: { pending: 0, enable: 1 , disable: 2 }
+
+  has_many :product_images, inverse_of: :product
+  accepts_nested_attributes_for :product_images, reject_if: :all_blank, allow_destroy: true
 end
